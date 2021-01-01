781132b81c19ad9f15e6ec7f226a4e842c64bdbc
hmy2001 <harutowii@yahoo.co.jp>
2021-01-01 21:21:46 +0900

    Updated

--- src/GenerateStructureToWorld/Main.php
+++ src/GenerateStructureToWorld/Main.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace GenerateStructureToWorld;
 
+use pocketmine\block\Block;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\nbt\LittleEndianNBTStream;
@@ -58,7 +59,18 @@ class Main extends PluginBase{
 		];
 		$this->cobbleStoneWallData = [
 			"cobblestone",
-			"mossy_cobblestone"
+			"mossy_cobblestone",
+			"granite",
+			"diorite",
+			"andesite",
+			"sandstone",
+			"brick",
+			"stone_brick",
+			"mossy_stone_brick",
+			"end_brick",
+			"prismarine",
+			"red_sandstone",
+			"red_nether_brick",
 		];
 		$this->stoneSlabTypeData = [
 			"smooth_stone",
@@ -121,10 +133,10 @@ class Main extends PluginBase{
 	}
 
 	private function generateStructure(string $directoryName, string $directory){//TODO: startX, startY, startZ
-		$this->getServer()->broadcastMessage("Generating structure json: ".$directoryName);
-		$startX = 300;
-		$startY = 50;
-		$startZ = 255;
+		$this->getServer()->broadcastMessage("Generating structure: ".$directoryName);
+		$startX = 0;
+		$startY = 5;
+		$startZ = 0;
 		$structureData = $this->getStructureData($directory);
 		foreach($structureData as $data){
 			$x = $startX + $data[0];
@@ -135,8 +147,9 @@ class Main extends PluginBase{
 			//$this->getServer()->getDefaultLevel()->setBlockLightAt($x, $y, $z, 15);
 			//$this->getServer()->getDefaultLevel()->setBlockSkyLightAt($x, $y, $z, 15);
 			//TODO: update light
+			//todo: add title
 		}
-		$this->getServer()->broadcastMessage("Generated structure json: ".$directoryName);
+		$this->getServer()->broadcastMessage("Generated structure: ".$directoryName);
 	}
 
 	private function createStructureJson(string $directoryName, string $directory){
@@ -174,8 +187,15 @@ class Main extends PluginBase{
 					for($z = 0; $z < $nbtData["size"][2]; $z++){
 						$paletteIndex = $nbtData["structure"]["block_indices"][0][$index];
 						$paletteData = $nbtData["structure"]["palette"]["default"]["block_palette"][$paletteIndex];
+						//$blockEntityData = $nbtData["structure"]["palette"]["default"]["block_position_data"][$index]["block_entity_data"];
+						/*if(isset($nbtData["structure"]["palette"]["default"]["block_position_data"][$index]["block_entity_data"])){
+							var_dump($nbtData["structure"]["palette"]["default"]["block_position_data"][$index]["block_entity_data"]);
+						}*/
+
 						$blockData = $this->getBlockDataFromBlockPalette($paletteData);
-						$structureData[] = [$startX + $x, $startY + $y, $startZ + $z, $blockData[0], $blockData[1]];
+						if($blockData[0] !== Block::AIR){
+							$structureData[] = [$startX + $x, $startY + $y, $startZ + $z, $blockData[0], $blockData[1]];
+						}
 						$index++;
 					}
 				}
@@ -199,7 +219,6 @@ class Main extends PluginBase{
 	}
 
 	private function getBlockDataFromBlockPalette(array $paletteData): array{
-		$blockId = 0;
 		$blockDamage = 0;
 		$blockName = $paletteData["name"];
 		if($blockName === "minecraft:structure_block"){
@@ -207,105 +226,374 @@ class Main extends PluginBase{
 		}else{
 			if(isset($this->legacyIdMap[$blockName])){
 				$blockId = $this->legacyIdMap[$blockName];
-				switch($blockName){
-					case "minecraft:stone":
-						if(isset($paletteData["states"]["stone_type"])){
-							if(($key = array_search($paletteData["states"]["stone_type"], $this->stoneTypeData, true)) !== false){
-								$blockDamage = $key;
-							}else{
-								var_dump([$blockName => $paletteData["states"]]);
+				if($blockId > 255){//255よりも大きい
+					if($blockName === "minecraft:barrier"){
+						$blockId = Block::INVISIBLE_BEDROCK;
+					}else{
+						echo $blockName."\n";
+						$blockId = Block::RESERVED6;//update texture block
+					}
+				}else{
+					switch($blockName){
+						case "minecraft:stone":
+							if(isset($paletteData["states"]["stone_type"])){
+								if(($key = array_search($paletteData["states"]["stone_type"], $this->stoneTypeData, true)) !== false){
+									$blockDamage = $key;
+								}else{
+									var_dump([$blockName => $paletteData["states"]]);
+								}
 							}
-						}
-					break;
-					case "minecraft:dirt":
-						if(isset($paletteData["states"]["dirt_type"])){
-							if($paletteData["states"]["dirt_type"] === "normal"){
-								$blockDamage = 0;
-							}else{
-								var_dump([$blockName => $paletteData["states"]]);
+						break;
+						case "minecraft:dirt":
+							if(isset($paletteData["states"]["dirt_type"])){
+								if($paletteData["states"]["dirt_type"] === "normal"){
+									$blockDamage = 0;
+								}else{
+									var_dump([$blockName => $paletteData["states"]]);
+								}
 							}
-						}
-					break;
-					case "minecraft:water":
-					case "minecraft:lava":
-						/*if(isset($paletteData["states"]["liquid_depth"])){
-							//TODO: implement it
-						}*/
-					break;
-					case "minecraft:wool":
-					case "minecraft:stained_hardened_clay":
-						if(isset($paletteData["states"]["color"])){
-							if(($key = array_search($paletteData["states"]["color"], $this->colorData, true)) !== false){
-								$blockDamage = $key;
-							}else{
-								var_dump([$blockName => $paletteData["states"]]);
+						break;
+						case "minecraft:water":
+						case "minecraft:lava":
+							/*if(isset($paletteData["states"]["liquid_depth"])){
+								//TODO: implement it
+							}*/
+						break;
+						case "minecraft:log2":
+							if(isset($paletteData["states"]["stone_brick_type"])){
+								switch($paletteData["states"]["stone_brick_type"]){
+									case "acacia":
+										$blockDamage = 0;
+									break;
+									case "dark_oak":
+										$blockDamage = 1;
+									break;
+									default:
+										var_dump([$blockName => $paletteData["states"]]);
+									break;
+								}
 							}
-						}
-					break;
-					case "minecraft:wooden_slab":
-					case "minecraft:fence":
-					case "minecraft:planks":
-					case "minecraft:leaves":
-						if(isset($paletteData["states"]["wood_type"])){
-							if(($key = array_search($paletteData["states"]["wood_type"], $this->woodColorData, true)) !== false){
-								$blockDamage = $key;
-							}else{
-								var_dump([$blockName => $paletteData["states"]]);
+
+							//TODO: convert axis
+							/*if(isset($paletteData["states"]["pillar_axis"])){
+								switch($paletteData["states"]["pillar_axis"]){
+									case "x":
+
+									break;
+									case "y":
+
+									break;
+									case "z":
+
+									break;
+								}
+							}*/
+						break;
+						case "minecraft:wool":
+						case "minecraft:stained_hardened_clay":
+						case "minecraft:concrete":
+						case "minecraft:carpet":
+						case "minecraft:stained_glass_pane":
+							if(isset($paletteData["states"]["color"])){
+								if(($key = array_search($paletteData["states"]["color"], $this->colorData, true)) !== false){
+									$blockDamage = $key;
+								}else{
+									var_dump([$blockName => $paletteData["states"]]);
+								}
 							}
-						}
-					break;
-					case "minecraft:stone_slab":
-						if(isset($paletteData["states"]["stone_slab_type"])){
-							if(($key = array_search($paletteData["states"]["stone_slab_type"], $this->stoneSlabTypeData, true)) !== false){
-								$blockDamage = $key;
-							}else{
-								var_dump([$blockName => $paletteData["states"]]);
+						break;
+						case "minecraft:fence":
+						case "minecraft:planks":
+						case "minecraft:leaves":
+							if(isset($paletteData["states"]["wood_type"])){
+								if(($key = array_search($paletteData["states"]["wood_type"], $this->woodColorData, true)) !== false){
+									$blockDamage = $key;
+								}else{
+									var_dump([$blockName => $paletteData["states"]]);
+								}
 							}
-						}
-						//TODO: top bit
-					break;
-					case "minecraft:stone_slab2":
-						if(isset($paletteData["states"]["stone_slab_type_2"])){
-							if($paletteData["states"]["stone_slab_type_2"] === "mossy_cobblestone"){
-								$blockDamage = 5;
-							}else{
-								var_dump([$blockName => $paletteData["states"]]);
+						break;
+						case "minecraft:wooden_slab":
+						case "minecraft:double_wooden_slab":
+							if(isset($paletteData["states"]["wood_type"])){
+								if(($key = array_search($paletteData["states"]["wood_type"], $this->woodColorData, true)) !== false){
+									$blockDamage = $key;
+								}else{
+									var_dump([$blockName => $paletteData["states"]]);
+								}
 							}
-						}
-						//TODO; top bit
-					break;
-					case "minecraft:cobblestone_wall":
-						if(isset($paletteData["states"]["wall_block_type"])){
-							if(($key = array_search($paletteData["states"]["wall_block_type"], $this->cobbleStoneWallData, true)) !== false){
-								$blockDamage = $key;
-							}else{
-								var_dump([$blockName => $paletteData["states"]]);
+
+							if(isset($paletteData["states"]["top_slot_bit"])){
+								if($paletteData["states"]["top_slot_bit"] === 1){
+									$blockDamage |= 0x08;
+								}
 							}
-						}
-					break;
-					case "minecraft:smooth_stone"://TODO: convert
-						//$blockId = 434;
-						//$blockDamage = 0;
-					break;
-					case "minecraft:mossy_cobblestone_stairs"://TODO: convert
-						//$blockId = 434;
-						var_dump([$blockName => $paletteData["states"]]);
-					break;
-					case "minecraft:quartz_block":
-						//TODO: convert
-					break;
-					case "minecraft:coral":
-					case "minecraft:kelp":
-						$blockId = 0;
-					break;
-					case "minecraft:bedrock":
-					break;
-					default:
-						if(!empty($paletteData["states"])){
+						break;
+						case "minecraft:stone_slab":
+						case "minecraft:double_stone_slab":
+							if(isset($paletteData["states"]["stone_slab_type"])){
+								if(($key = array_search($paletteData["states"]["stone_slab_type"], $this->stoneSlabTypeData, true)) !== false){
+									$blockDamage = $key;
+								}else{
+									var_dump([$blockName => $paletteData["states"]]);
+								}
+							}
+
+							if(isset($paletteData["states"]["top_slot_bit"])){
+								if($paletteData["states"]["top_slot_bit"] === 1){
+									$blockDamage |= 0x08;
+								}
+							}
+						break;
+						case "minecraft:stone_slab2":
+							if(isset($paletteData["states"]["stone_slab_type_2"])){
+								if($paletteData["states"]["stone_slab_type_2"] === "mossy_cobblestone"){
+									$blockDamage = 5;
+								}else{
+									var_dump([$blockName => $paletteData["states"]]);
+								}
+							}
+
+							if(isset($paletteData["states"]["top_slot_bit"])){
+								if($paletteData["states"]["top_slot_bit"] === 1){
+									$blockDamage |= 0x08;
+								}
+							}
+						break;
+						case "minecraft:cobblestone_wall":
+							if(isset($paletteData["states"]["wall_block_type"])){
+								if(($key = array_search($paletteData["states"]["wall_block_type"], $this->cobbleStoneWallData, true)) !== false){
+									$blockDamage = $key;
+								}else{
+									var_dump([$blockName => $paletteData["states"]]);
+								}
+							}
+						break;
+						case "minecraft:smooth_stone"://TODO: convert
+							//$blockId = 434;
+							//$blockDamage = 0;
 							var_dump([$blockName => $paletteData["states"]]);
-						}
-					break;
+						break;
+						case "minecraft:mossy_cobblestone_stairs"://TODO: convert
+							//$blockId = 434;
+							var_dump([$blockName => $paletteData["states"]]);
+						break;
+						case "minecraft:spruce_stairs":
+						case "minecraft:brick_stairs":
+						case "minecraft:quartz_stairs":
+							if(isset($paletteData["states"]["weirdo_direction"])){
+								$blockDamage = $paletteData["states"]["weirdo_direction"];
+							}
+
+							if(isset($paletteData["states"]["upside_down_bit"])){
+								if($paletteData["states"]["upside_down_bit"] === 1){
+									$blockDamage |= 0x04;
+								}
+							}
+						break;
+						case "minecraft:quartz_block":
+							if(isset($paletteData["states"]["stone_brick_type"])){
+								switch($paletteData["states"]["stone_brick_type"]){
+									case "default":
+										$blockDamage = 0;
+									break;
+									case "chiseled":
+										$blockDamage = 1;
+									break;
+									case "lines":
+										if(isset($paletteData["states"]["pillar_axis"])){
+											switch($paletteData["states"]["pillar_axis"]){
+												case "x":
+													$blockDamage = 4;
+												break;
+												case "y":
+													$blockDamage = 2;
+												break;
+												case "z":
+													$blockDamage = 3;
+												break;
+											}
+										}
+									break;
+									case "smooth":
+										//TODO: 分からん
+										var_dump([$blockName => $paletteData["states"]]);
+									break;
+									default:
+										var_dump([$blockName => $paletteData["states"]]);
+									break;
+								}
+							}
+						break;
+						case "minecraft:prismarine":
+							if(isset($paletteData["states"]["prismarine_block_type"])){
+								switch($paletteData["states"]["prismarine_block_type"]){
+									case "default":
+										$blockDamage = 0;
+									break;
+									case "dark":
+										$blockDamage = 1;
+									break;
+									case "bricks":
+										$blockDamage = 2;
+									break;
+									default:
+										var_dump([$blockName => $paletteData["states"]]);
+									break;
+								}
+							}
+						break;
+						case "minecraft:anvil":
+						case "minecraft:chest":
+						case "minecraft:bed":
+							/*if(isset($paletteData["states"]["damage"])){//todo: this is anvil process
+								switch($paletteData["states"]["damage"]){
+									case "undamaged":
+										$blockDamage = 0;
+									break;
+									case "slightly_damaged":
+										$blockDamage = 4;
+									break;
+									case "very_damaged":
+										$blockDamage = 8;
+									break;
+									case "broken":
+										$blockDamage = 12;
+									break;
+									default:
+										var_dump([$blockName => $paletteData["states"]]);
+									break;
+								}
+							}
+
+							if(isset($paletteData["states"]["direction"])){
+								//TODO: convert
+							}*/
+							$blockId = 0;//TODO: Tileをなんとかしないといけない
+						break;
+						case "minecraft:stonebrick":
+							if(isset($paletteData["states"]["stone_brick_type"])){
+								switch($paletteData["states"]["stone_brick_type"]){
+									case "default":
+										$blockDamage = 0;
+									break;
+									case "mossy":
+										$blockDamage = 1;
+									break;
+									case "cracked":
+										$blockDamage = 2;
+									break;
+									case "chiseled":
+										$blockDamage = 3;
+									break;
+									case "smooth":
+										$blockDamage = 4;
+									break;
+									default:
+										var_dump([$blockName => $paletteData["states"]]);
+									break;
+								}
+							}
+						break;
+						case "minecraft:cauldron":
+							if(isset($paletteData["states"]["fill_level"])){
+								switch($paletteData["states"]["fill_level"]){
+									case 0:
+										$blockDamage = 0;
+									break;
+									case 1:
+									case 2:
+										$blockDamage = 1;
+									break;
+									case 3:
+									case 4:
+										$blockDamage = 2;
+									break;
+									case 5:
+									case 6:
+										$blockDamage = 3;
+									break;
+									default:
+										var_dump([$blockName => $paletteData["states"]]);
+									break;
+								}
+							}
+
+							if(isset($paletteData["states"]["cauldron_liquid"])){
+								if($paletteData["states"]["cauldron_liquid"] !== "water"){
+									$blockId = Block::RESERVED6;//update texture block
+									$blockDamage = 0;
+									var_dump([$blockName => $paletteData["states"]]);
+								}
+							}
+						break;
+						/*case "minecraft:yellow_glazed_terracotta":
+						case "minecraft:piston":
+							if(isset($paletteData["states"]["facing_direction"])){
+								switch($paletteData["states"]["facing_direction"]){
+									case 0:
+
+									break;
+									case 1:
+
+									break;
+									case 2:
+
+									break;
+									case 3:
+
+									break;
+									case 4:
+
+									break;
+									case 5:
+
+									break;
+									default:
+										var_dump([$blockName => $paletteData["states"]]);
+									break;
+								}
+							}
+						break;*/
+						case "minecraft:torch":
+							if(isset($paletteData["states"]["torch_facing_direction"])){
+								switch($paletteData["states"]["torch_facing_direction"]){
+									case "west":
+										$blockDamage = 1;
+									break;
+									case "east":
+										$blockDamage = 2;
+									break;
+									case "north":
+										$blockDamage = 3;
+									break;
+									case "south":
+										$blockDamage = 4;
+									break;
+									case "top":
+										$blockDamage = 5;
+									break;
+									default:
+										var_dump([$blockName => $paletteData["states"]]);
+									break;
+								}
+							}
+						break;
+						case "minecraft:pistonArmCollision":
+							$blockId = 0;
+						break;
+						case "minecraft:flower_pot":
+						case "minecraft:bedrock":
+						break;
+						default:
+							if(!empty($paletteData["states"])){
+								var_dump([$blockName => $paletteData["states"]]);
+							}
+						break;
+					}
 				}
+			}else{
+				$blockId = Block::RESERVED6;//update texture block
 			}
 		}
 
